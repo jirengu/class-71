dcb3462b50087da03e157e284826d4a6a7610f4f
jirengu <hunger@jirengu.com>
2022-11-04 15:42:56 +0800

    javascript

---
+++ JavaScript-ch1/demo2/index.js
@@ -0,0 +1,35 @@
+// let str = 'jirengu';
+// console.log(str);
+
+// function fn() {
+//   let undefined = 100;
+//   let a = void 0
+//   console.log(a)
+// }
+// fn()
+
+// let a = 1, b = 2;
+// console.log(a, b)
+
+// let c = (3, 4)
+// console.log(c)
+
+// let d = a = 3
+// console.log(d)
+
+typeof 2+3  //"number3"
+typeof (2+3)  //"number"
+typeof typeof 2+3 //"string3"
+
+let a = 2, b = 3
+console.log(a+++b)  //5
+
+let c = d = 5
+console.log(c)     //5
+let x = (a = 1, b = 2)
+console.log(x, a, b)  //2 1 2
+
+let num = 50
+let isOk = false
+let result = !isOk&&num+10>60  //(!isOk) && ((num+10)>60)  false
+
